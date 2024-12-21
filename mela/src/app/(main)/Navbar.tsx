96f5a76e5f73160ec8a51dfc3766b16e20f9b268
cigerxwinislam <islamtunc748@gmail.com>
2024-12-22 00:58:06 +0300

    serxere nu

--- mela/src/app/(main)/Navbar.tsx
+++ mela/src/app/(main)/Navbar.tsx
@@ -12,35 +12,17 @@ import { redirect } from "next/navigation";
 
 
 
-import { useState } from "react";
-
-
-
-const [mmkes, setmmkes] = useState(false)
-
-
-
-
-
-const session= await validateRequest()
-
-if (!session.user)setmmkes(true);
-
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-10 bg-card shadow-sm">
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
         <Link href="/" className="text-2xl font-bold text-primary">
           
-          Müşterisi Burada
+          Ferhenga Mela
         </Link>
 
 
-{
 
-
-
-}
         <SearchField />
 
       </div>
