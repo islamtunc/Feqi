a58bc733527bbe6693f353b65c6db3c4c11ba95d
mmrxistin <mmrxistin@gmail.com>
2025-12-04 06:25:50 +0300

    Subhanallah, Elhamdulillah Allahu Ekber

--- mela/src/auth.ts
+++ mela/src/auth.ts
@@ -1,7 +1,7 @@
 // Bismillahirrahmanirahim
 // Elhamdulillahirrabbulalemin
 // Esselatu vesselamu ala seyyidina Muhammedin ve ala alihi ve sahbihi ecmain
-// Subhanallah, Elhamdulillah, Allahu Ekber
+// Subhanallah, Elhamdulillah, RAHIM-I Hamid Allahu Ekber
 // La ilahe illallah 
 // Allahu Ekber, Allahu Ekber, Allahu Ekber, La ilahe illallah
 // Bila Allah Azze ve Celle me ji sunneta Resulullah Muhammed (s.a.v) neqetine, amin rabbal alemin 
