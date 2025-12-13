daf0f8cb32daae5b64775add05e9a8d22e302a55
mmrxistin <mmrxistin@gmail.com>
2025-12-13 21:21:01 +0300

    Elhamdulillahirabbulalemin

--- mela/src/app/malper/fiqih/posts/[postId]/page.tsx
+++ mela/src/app/malper/fiqih/posts/[postId]/page.tsx
@@ -1,11 +1,14 @@
 // Bismillahirrahmanirrahim
-// Elhamdulillahi Rabbil Alamin
-// Essalatu vesselamu ala Resulina Muhammedin ve ala alihi ve sahbihi ecmain
-// Allah U Ekber, Allah U Ekber, Allah U Ekber, La ilahe illallah
-// Subhanallah, Elhamdulillah, Allahu Ekber
+// Elhamdulillahirabbulalemin
+// Es-selatu vesselamu ala resulina Muhammedin 
+// Allah u Ekber, Allah u Ekber, Allah u Ekber, La ilahe illallah
+// SuphanAllah, Elhamdulillah, Allahu Ekber
+// La ilahe illallah
+
+import { validateRequest } from "@/auth";
+
 import Linkify from "@/components/Linkify";
-import MmmPost from "@/components/agahi/mmPost";
-import Post from "@/components/agahi/mmPost";
+import MmmPost from "@/components/dirok/mmPost";
 import UserAvatar from "@/components/UserAvatar";
 import UserTooltip from "@/components/UserTooltip";
 import prisma from "@/lib/prisma";
@@ -20,12 +23,12 @@ interface PageProps {
   params: { postId: string };
 }
 
-const getPost = cache(async (postId: string) => {
+const getPost = cache(async (postId: string, loggedInUserId: string) => {
   const post = await prisma.fiqih.findUnique({
     where: {
       id: postId,
     },
-    include: getPostDataInclude(""), // Kullanıcı doğrulama kaldırıldı
+    include: getPostDataInclude(loggedInUserId),
   });
 
   if (!post) notFound();
@@ -33,23 +36,42 @@ const getPost = cache(async (postId: string) => {
   return post;
 });
 
+export async function generateMetadata({
+  params: { postId },
+}: PageProps): Promise<Metadata> {
+  const { user } = await validateRequest();
+
+  if (!user) return {};
+
+  const post = await getPost(postId, user.id);
+
+  return {
+    title: `${post.user.displayName}: ${post.content.slice(0, 50)}...`,
+  };
+}
 
 export default async function Page({ params: { postId } }: PageProps) {
-  const post = await getPost(postId);
+  const { user } = await validateRequest();
+
+  if (!user) {
+    return (
+      <p className="text-destructive">
+        You&apos;re not authorized to view this page.
+      </p>
+    );
+  }
+
+  const post = await getPost(postId, user.id);
 
   return (
-    <main 
-      className="flex flex-col lg:flex-row w-full min-h-screen bg-gradient-to-br from-[#f8fafc] to-[#e2e8f0] p-4"
-    >
-      {/* İçerik Kartı */}
-      <div className="flex-1 flex justify-center items-start">
-        <div className="w-full max-w-2xl">
-          <MmmPost post={post} />
-          {/* Ayraç ve boşluk */}
-          <div className="my-8">
-            <hr className="border-t-4 border-gray-355" />
-          </div>
-        </div>
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
+        <MmmPost post={post} />
+      </div>
+      <div className="sticky top-[5.25rem] hidden h-fit w-80 flex-none lg:block">
+        <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
+          <UserInfoSidebar user={post.user} />
+        </Suspense>
       </div>
     </main>
   );
@@ -59,15 +81,36 @@ interface UserInfoSidebarProps {
   user: UserData;
 }
 
+async function UserInfoSidebar({ user }: UserInfoSidebarProps) {
+  const { user: loggedInUser } = await validateRequest();
 
+  if (!loggedInUser) return null;
 
-
-
-
-
-
-
-
-
-
-
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">About this user</div>
+      <UserTooltip user={user}>
+        <Link
+          href={`/users/${user.username}`}
+          className="flex items-center gap-3"
+        >
+          <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+          <div>
+            <p className="line-clamp-1 break-all font-semibold hover:underline">
+              {user.displayName}
+            </p>
+            <p className="line-clamp-1 break-all text-muted-foreground">
+              @{user.username}
+            </p>
+          </div>
+        </Link>
+      </UserTooltip>
+      <Linkify>
+        <div className="line-clamp-6 whitespace-pre-line break-words text-muted-foreground">
+          {user.bio}
+        </div>
+      </Linkify>
+    
+    </div>
+  );
+}
